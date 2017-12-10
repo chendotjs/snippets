aa3a67f84e0b1faac34528bbd37998478c700def
chendotjs <chendotjs@qq.com>
2017-12-10 16:41:08 +0800

    add epoll_demo_lt.c

--- io_multiplex/epoll_demo_lt.c
+++ io_multiplex/epoll_demo_lt.c
@@ -1,7 +1,7 @@
 #include <fcntl.h>
-#include <poll.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/epoll.h>
 #include <sys/time.h>
 #include <sys/types.h>
 #include <unistd.h>
@@ -12,38 +12,55 @@
     exit(x);                                                                   \
   } while (0)
 
+#define MAKE_EVENT(NAME, IDX)                                                  \
+  struct epoll_event NAME##IDX;                                                \
+  (NAME##IDX).events = EPOLLIN;                                                \
+  (NAME##IDX).data.fd = fd##IDX;                                               \
+  epoll_ctl(epoll_fd, EPOLL_CTL_ADD, fd##IDX, &(NAME##IDX));
+
+#define EPOLLEVENTS 10
+
 void showdata(const char *fname, int fd);
 
 int main(int argc, char const *argv[]) {
   int fd1, fd2;
-  struct pollfd fds[2];
+  int epoll_fd;
   int retval;
   int timeout;
+
   if (argc != 4) {
     fprintf(stderr, "usage:%s file file timeout\n", argv[0]);
     exit(1);
   }
-  
+
   timeout = 1000 * atoi(argv[3]);
+
+  epoll_fd = epoll_create1(0);
+  if (epoll_fd == -1)
+    oops("epoll_create1", 1);
 
   if ((fd1 = open(argv[1], O_RDONLY)) == -1)
     oops(argv[1], 2);
   if ((fd2 = open(argv[2], O_RDONLY)) == -1)
     oops(argv[2], 3);
-  fds[0].fd = fd1;
-  fds[1].fd = fd2;
-  fds[0].events = POLLIN;
-  fds[1].events = POLLIN;
+
+  MAKE_EVENT(event, 1);
+  MAKE_EVENT(event, 2);
+
+  struct epoll_event events[EPOLLEVENTS];
 
   while (1) {
-    retval = poll(fds, sizeof(fds) / sizeof(struct pollfd), timeout);
+    retval = epoll_wait(epoll_fd, events, EPOLLEVENTS, timeout);
     if (retval == -1)
-      oops("poll", 4);
+      oops("epoll", 4);
     if (retval > 0) {
-      if (fds[0].revents & POLLIN)
-        showdata(argv[1], fd1);
-      if (fds[1].revents & POLLIN)
-        showdata(argv[2], fd2);
+      for (int i = 0; i < retval; i++) {
+        int fd = events[i].data.fd;
+        const char *device = fd == fd1 ? argv[1] : argv[2];
+        if (events[0].events & EPOLLIN) {
+          showdata(device, fd);
+        }
+      }
     } else
       printf("no input buffer after %d seconds\n", atoi(argv[3]));
   }
